bb9905ab9bece3d9192791b3bbc2f374dea3360e
Rudolf Winestock <rudolf@winestockwebdesign.com>
2017-06-22 17:57:57 -0400

    Create timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -9,8 +9,33 @@ var Timer = React.createClass({
       timerStatus: 'stopped'
     };
   },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.timerStatus !== prevState.timerStatus) {
+      switch (this.state.timerStatus) {
+        case 'started':
+        this.handleStart();
+        break;
+        case 'stopped':
+        this.setState({count: 0});
+        case 'paused':
+        clearInterval(this.timer);
+        this.timer = undefined;
+        break;
+      }
+    }
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+  },
+  handleStart: function () {
+    this.timer = setInterval(() => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    }, 1000);
+  },
   handleStatusChange: function (newTimerStatus) {
-    console.log(newTimerStatus);
+    this.setState({timerStatus: newTimerStatus});
   },
   render: function () {
     var {count, timerStatus} = this.state;
